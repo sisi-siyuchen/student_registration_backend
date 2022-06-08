e3347114216b327ef34092dc6d6c8010d19c2090
Siyu Chen <siyuchen@Siyus-MacBook-Pro.local>
2022-06-07 18:09:46 -0700

    Initial version of course_enroll_backend generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/github/sisi/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.github.sisi.repository;
+
+import com.github.sisi.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
